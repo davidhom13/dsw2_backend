5c04d6c7bb0b8460a6f72643b469f51c714e72fc
Juan9207 <tato9207@gmail.com>
2022-07-01 17:36:57 -0500

    listaProgramaPorIdCategoriaNombrePrecio comentado

--- src/main/java/com/proyecto/service/ProgramaService.java
+++ src/main/java/com/proyecto/service/ProgramaService.java
@@ -12,7 +12,7 @@ public interface ProgramaService {
 	public abstract List<Programa> listaProgramaPorId(String id_programa);
 	public abstract Programa RegistraPrograma(Programa obj);
 	
-	public List<Programa> listaProgramaPorIdCategoriaNombrePrecio(int id_programa, int id_categoria, String nom_prog, String precio);
+	/*public List<Programa> listaProgramaPorIdCategoriaNombrePrecio(int id_programa, int id_categoria, String nom_prog, String precio);*/
 	
 	public abstract List<Programa> listaProgramaPorNombreLike(String nom_prog);
 	public abstract Programa insertaActualizaPrograma(Programa obj);
