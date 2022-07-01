498521d41c0c31cc8b4a63aa85a3adbc60c12a64
davidhom13 <davidhugo.ortiz@gmail.com>
2022-06-30 23:39:51 -0500

    Listado Tipo

---
+++ src/main/java/com/proyecto/repository/TipoPartesRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.proyecto.entidad.TipoPartes;
+
+public interface TipoPartesRepository extends JpaRepository<TipoPartes, Integer>{
+
+}
